d36dad8c220ce8d3017dfe029957dce250c2c70d
stanleyyork <stanleyyork@gmail.com>
2015-12-15 14:18:28 -0800

    init commit

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -10,13 +10,34 @@ class PetsController < ApplicationController
 
   def create
     pet_params = params.require(:pet).permit(:name, :breed)
-    pet = Pet.create(pet_params)
-    redirect_to pet_path(pet)
+    pet = Pet.new(pet_params)
+    if pet.save
+      redirect_to pet_path(pet)
+    else
+      flash[:error] = pet.errors.full_messages.join(", ")
+      redirect_to new_pet_path
+    end
+    
   end
 
   def show
     pet_id = params[:id]
     @pet = Pet.find_by_id(pet_id)
+  end
+
+  def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def update
+    @pet = Pet.find(params[:id])
+    pet_params = params.require(:pet).permit(:name, :breed)
+    if @pet.update_attributes(pet_params)
+      redirect_to pet_path(@pet)
+    else
+      flash[:error] = @pet.errors.full_messages.join(", ")
+      redirect_to edit_pet_path
+    end
   end
 
 end
